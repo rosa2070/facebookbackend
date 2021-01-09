0bd29946e50190b52120853163f781200c8ad75a
jaeeunhyun <rosa2070@naver.com>
2021-01-09 14:04:06 +0900

    print("hihihi")

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+import re
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = model.CharField('별명', max_length=30, unique=True)
+    ProcessedImageField(upload_to=user_path,
+                        )
