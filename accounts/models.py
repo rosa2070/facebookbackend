5946d51793ab3d52e1faad92cf5e58539ba92b3e
jaeeunhyun <rosa2070@naver.com>
2021-01-09 15:06:02 +0900

    print("two")

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,34 @@ from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 import re
 
+
+def user_path(instance, filename):
+    from random import choice
+    import string
+    arr = [choice(string.ascii_letters) for _ in range(8)]
+    pid = ''.join(arr)
+    extension = filename.split('.')[-1]
+    return 'accounts/{}/{}.{}'.format(instance.user.username, pid, extension)
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    nickname = model.CharField('별명', max_length=30, unique=True)
-    ProcessedImageField(upload_to=user_path,
-                        )
+    nickname = models.CharField('별명', max_length=30, unique=True)
+    picture = ProcessedImageField(upload_to=user_path,
+                                    processors=[ResizeToFill(150, 150)],
+                                    format='JPEG',
+                                    options={'quality': 90},
+                                    blank=True,
+                                    )
+
+    about = models.CharField(max_length=300, blank=True)
+
+    GENDER_C = (
+        ('선택안함', '선택안함'),
+        ('여성', '여성'),
+        ('남성', '남성'),
+    )
+
+    gender = models.CharField('성별(선택사항)',
+                                 max_length=10,
+                                 choices=GENDER_C,
+                                 default='N')
