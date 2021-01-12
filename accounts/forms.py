1603ba478ef2f520e37b63210946e3653a2607ef
jaeeunhyun <rosa2070@naver.com>
2021-01-12 19:07:24 +0900

    print("five")

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+    username = forms.CharField(label='사용자명', widget=forms.TextInput(attrs={
+        'pattern': '[a-zA-Z0-9]+',
+        'title': '특수문자, 공백 입력불가',
+    }))
+
+    nickname = forms.CharField(label='닉네임')
+    picture = forms.ImageField(label='프로필사진', required=False)
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
